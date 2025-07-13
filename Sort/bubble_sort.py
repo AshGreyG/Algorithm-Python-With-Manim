49914fff7a997d5b6b9a81e64b231d3e1940f5b1
AshGrey🥕 <ashgrey.huaier@gmail.com>
2025-07-13 20:27:55 +0800

    :see_no_evil: chore(gitignore): add .gitignore to hide pycache and venv

--- Sort/bubble_sort.py
+++ Sort/bubble_sort.py
@@ -11,3 +11,15 @@ def bubble_sort(array : List[T]) -> NoReturn :
             if array[i - 1] > array[i] :
                 array[i], array[i - 1] = array[i - 1], array[i]
                 swapped = True
+
+def memorized_bubble_sort(array : List[T]) -> NoReturn :
+    n = len(array)
+    j = len(array)
+    swapped = True
+    while swapped == True :
+        swapped = False
+        for i in range(1, j) :
+            if array[i - 1] > array[i] :
+                array[i], array[i - 1] = array[i - 1], array[i]
+                swapped = True
+                j = i
