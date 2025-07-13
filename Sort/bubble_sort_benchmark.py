4aabdea7dd6a49152f754cc14e8e9b851e0affe6
AshGrey🥕 <ashgrey.huaier@gmail.com>
2025-07-13 23:12:17 +0800

    :sparkles: feat(benchmark): add memory benchmark for bubble sort

--- Sort/bubble_sort_benchmark.py
+++ Sort/bubble_sort_benchmark.py
@@ -1,6 +1,19 @@
-from utils.simp_benchmark import function_timer
-from .bubble_sort import bubble_sort, memorized_bubble_sort
-from typing import List, NoReturn, TypeVar, Tuple, Callable
+from utils.simp_benchmark import (
+    function_timer, 
+    function_memory_monitor
+)
+from .bubble_sort import (
+    bubble_sort, 
+    memorized_bubble_sort
+)
+from typing import (
+    List, 
+    NoReturn, 
+    TypeVar, 
+    Tuple, 
+    Callable
+)
+from utils.color import CatppuccinCoolColor, CatppuccinWarmColor
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,8 +21,6 @@ import matplotlib.font_manager as fm
 
 font_path = "/usr/share/fonts/libertinus/LibertinusSerif-Regular.ttf"
 libertinus_font = fm.FontProperties(fname = font_path)
-
-plt.figure(figsize = (6, 6))
 
 T = TypeVar("T")
 Func = TypeVar("Func", bound = Callable[List[T], NoReturn])
@@ -20,13 +31,26 @@ GEN_MAXIMAL = 100
 BENCH_MINIMAL = 200
 BENCH_MAXIMAL = 450
 BENCH_STEP = 5
-FAST_COMPLEXITY_SCALE = 150
+TIME_COMPLEXITY_SCALE = 150
+MEMO_COMPLEXITY_SCALE = 10000
+
+NORMAL_COLOR = CatppuccinWarmColor[
+    np.random.randint(0, len(CatppuccinWarmColor))
+]
+MEMORIZED_COLOR = CatppuccinCoolColor[
+    np.random.randint(0, len(CatppuccinCoolColor))
+]
 
 @function_timer
-def single_benchmark(array : List[T], func : Func) :
+def single_benchmark_time(array : List[T], func : Func) -> float :
     func(array)
 
-def dynamic_benchmark(func : Func) -> NoReturn :
+def dynamic_benchmark_time(
+    func : Func, 
+    color : str,
+    fill_between_label : str,
+    plot_label : str
+) -> NoReturn :
     """
     This is the benchmark function for bubble sort / memorized bubble sort.
 
@@ -47,7 +71,7 @@ def dynamic_benchmark(func : Func) -> NoReturn :
                 GEN_MAXIMAL,
                 size = size
             ).tolist()
-            average_bench_res.append(single_benchmark(arr, func))
+            average_bench_res.append(single_benchmark_time(arr, func))
 
         best_bench_res : List[float] = []
         for i in range(0, ONE_ROUND_TIMES) :
@@ -56,7 +80,7 @@ def dynamic_benchmark(func : Func) -> NoReturn :
                 GEN_MAXIMAL,
                 size = size
             ).tolist())
-            best_bench_res.append(single_benchmark(arr, func))
+            best_bench_res.append(single_benchmark_time(arr, func))
 
         worst_bench_res : List[float] = []
         for i in range(0, ONE_ROUND_TIMES) :
@@ -65,7 +89,7 @@ def dynamic_benchmark(func : Func) -> NoReturn :
                 GEN_MAXIMAL,
                 size = size
             ).tolist(), reverse = True)
-            worst_bench_res.append(single_benchmark(arr, func))
+            worst_bench_res.append(single_benchmark_time(arr, func))
 
         res.append((
             float(np.average(best_bench_res)),
@@ -74,20 +98,24 @@ def dynamic_benchmark(func : Func) -> NoReturn :
         ))
 
     min_res = list(map(lambda x : x[0], res))
-    avr_res = list(map(lambda x : x[1] / FAST_COMPLEXITY_SCALE, res))
-    max_res = list(map(lambda x : x[2] / FAST_COMPLEXITY_SCALE, res))
+    avr_res = list(map(lambda x : x[1] / TIME_COMPLEXITY_SCALE, res))
+    max_res = list(map(lambda x : x[2] / TIME_COMPLEXITY_SCALE, res))
 
     plt.fill_between(
         range(BENCH_MINIMAL, BENCH_MAXIMAL, BENCH_STEP),
         min_res,
         max_res,
+        color = color,
         alpha = 0.3,
-        linewidth = 0
+        linewidth = 0,
+        label = fill_between_label
     )
     plt.plot(
         range(BENCH_MINIMAL, BENCH_MAXIMAL, BENCH_STEP),
         avr_res,
-        alpha = 0.6
+        color = color,
+        alpha = 0.6,
+        label = plot_label
     )
     plt.xlabel(
         "Array Size",
@@ -108,7 +136,100 @@ def dynamic_benchmark(func : Func) -> NoReturn :
         fontsize = 12
     )
 
+@function_memory_monitor(interval = 0.0001)
+def single_benchmark_memory(array : List[T], func : Func) -> int :
+    func(array)
+
+def dynamic_benchmark_memory(
+    func : Func, 
+    color : str,
+    fill_between_label : str,
+    plot_label : str
+) -> NoReturn :
+    res : List[Tuple[float, float, float]] = []
+    for size in range(BENCH_MINIMAL, BENCH_MAXIMAL, BENCH_STEP) :
+        average_bench_res : List[float] = []
+        for i in range(0, ONE_ROUND_TIMES) :
+            arr = np.random.randint(
+                GEN_MINIMAL,
+                GEN_MAXIMAL,
+                size = size
+            ).tolist()
+            average_bench_res.append(single_benchmark_memory(arr, func))
+        res.append((
+            int(np.min(average_bench_res)),
+            int(np.average(average_bench_res)),
+            int(np.max(average_bench_res))
+        ))
+
+    min_res = list(map(lambda x : x[0], res))
+    avr_res = list(map(lambda x : x[1] / MEMO_COMPLEXITY_SCALE, res))
+    max_res = list(map(lambda x : x[2] / MEMO_COMPLEXITY_SCALE, res))
+
+    plt.fill_between(
+        range(BENCH_MINIMAL, BENCH_MAXIMAL, BENCH_STEP),
+        min_res,
+        max_res,
+        color = color,
+        alpha = 0.3,
+        linewidth = 0,
+        label = fill_between_label
+    )
+    plt.plot(
+        range(BENCH_MINIMAL, BENCH_MAXIMAL, BENCH_STEP),
+        avr_res,
+        color = color,
+        alpha = 0.6,
+        label = plot_label
+    )
+    plt.xlabel(
+        "Array Size",
+        fontproperties = libertinus_font,
+        fontsize = 14
+    )
+    plt.ylabel(
+        "Algorithm Execution Memory (× 10⁴)",
+        fontproperties = libertinus_font,
+        fontsize = 14
+    )
+    plt.xticks(
+        fontproperties = libertinus_font,
+        fontsize = 12
+    )
+    plt.yticks(
+        fontproperties = libertinus_font,
+        fontsize = 12
+    )
+
 if __name__ == "__main__" :
-    dynamic_benchmark(bubble_sort)
-    dynamic_benchmark(memorized_bubble_sort)
-    plt.savefig("Bubble-Sort-Benchmark.svg", format = "svg")
+    plt.figure(figsize = (6, 6))
+    dynamic_benchmark_time(
+        bubble_sort, 
+        NORMAL_COLOR,
+        "Normal-Range",
+        "Normal-Average"
+    )
+    dynamic_benchmark_time(
+        memorized_bubble_sort,
+        MEMORIZED_COLOR,
+        "Memorized-Range",
+        "Memorized-Average"
+    )
+    plt.legend(loc = "upper left", prop = libertinus_font)
+    plt.savefig("Bubble-Sort-Benchmark-Time.svg", format = "svg")
+
+    plt.figure(figsize = (6, 6))
+    dynamic_benchmark_memory(
+        bubble_sort, 
+        NORMAL_COLOR,
+        "Normal-Range",
+        "Normal-Average"
+    )
+    dynamic_benchmark_memory(
+        memorized_bubble_sort, 
+        MEMORIZED_COLOR,
+        "Memorized-Range",
+        "Memorized-Average"
+    )
+    plt.legend(loc = "upper left", prop = libertinus_font)
+    plt.savefig("Bubble-Sort-Benchmark-Memory.svg", format = "svg")
