03afbfac9f3a1505f3179de8511572ba464d7d31
AshGrey🥕 <ashgrey.huaier@gmail.com>
2025-07-13 13:04:57 +0800

    :sparkles: feat(sort): add bubble sort implementation

---
+++ Sort/bubble_sort.py
@@ -0,0 +1,13 @@
+from typing import TypeVar, List, NoReturn
+
+T = TypeVar("T")
+
+def bubble_sort(array : List[T]) -> NoReturn :
+    n = len(array)
+    swapped = True
+    while swapped == True :
+        swapped = False
+        for i in range(1, n) :
+            if array[i - 1] > array[i] :
+                array[i], array[i - 1] = array[i - 1], array[i]
+                swapped = True
