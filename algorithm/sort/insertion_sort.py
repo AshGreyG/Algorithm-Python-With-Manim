f7a9231f9cae72b969a37385eada9149bbf89083
AshGrey🥕 <ashgrey.huaier@gmail.com>
2025-11-02 22:54:53 +0800

    :sparkles: feat(sort): update insertion sort implementation

--- algorithm/sort/insertion_sort.py
+++ algorithm/sort/insertion_sort.py
@@ -14,25 +14,3 @@ def insertion_sort(array : List[T]) -> None :
             array[j - 1], array[j] = array[j], array[j - 1]
             j -= 1
         i += 1
-
-def binary_insertion_sort(array : List[T]) -> None :
-    i = 1
-    while i < len(array) :
-        lf = 0
-        rt = i - 1
-        pos = lf
-        while lf <= rt :
-            mid = (lf + rt) // 2
-            if array[mid] == array[i] :
-                pos = mid
-                break
-            elif array[mid] < array[i] :
-                lf = mid + 1
-            else :
-                rt = mid - 1
-            pos = lf
-        temp = array[i]
-        for j in range(i - 1, pos - 1, -1) :
-            array[j + 1] = array[j]
-        array[pos] = temp
-        i += 1
